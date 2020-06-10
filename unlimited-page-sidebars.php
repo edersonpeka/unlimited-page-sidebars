7d300647e87003a166e5dbdadb55e03d9829d756
edersonpeka <edersonpeka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2020-06-10 20:08:58 +0000

    fix: testando chave de array antes de usar
    git-svn-id: http://plugins.svn.wordpress.org/unlimited-page-sidebars/trunk@2321811 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- unlimited-page-sidebars.php
+++ unlimited-page-sidebars.php
@@ -251,6 +251,9 @@ class unlimited_page_sidebars {
     public static function save_postdata( $post_id ) {
         // verify this came from the our screen and with proper authorization,
         // because save_post can be triggered at other times
+        if ( !array_key_exists( 'pagesidebars_noncename', $_POST ) ) {
+            return $post_id;
+        }
         if ( !wp_verify_nonce( $_POST['pagesidebars_noncename'], plugin_basename(__FILE__) ) ) {
             return $post_id;
         }
